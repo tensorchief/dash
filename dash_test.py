5f895317a246f32d7f1bb1cfdae807f7da4a8575
silviar <silvia.b.reynolds@gmail.com>
2018-07-08 16:28:59 +0200

    using pandas data frame

--- dash_test.py
+++ dash_test.py
@@ -2,6 +2,13 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import pandas as pd
+import plotly.graph_objs as go
+
+df = pd.read_csv('https://gist.githubusercontent.com/chriddyp/' +
+                 '5d1ea79569ed194d432e56108a04d188/raw/' +
+                 'a9f9e8076b837d541398e999dcbac2b2826a81f8/' +
+                 'gdp-life-exp-2007.csv')
 
 app = dash.Dash()
 
@@ -16,12 +23,26 @@ app.layout = html.Div(children=[
         id='example-graph',
         figure={
             'data': [
-                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
-                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
+                go.Scatter(
+                    x=df[df['continent'] == i]['gdp per capita'],
+                    y=df[df['continent'] == i]['population'],
+                    text=df[df['continent'] == i]['country'],
+                    mode='markers',
+                    opacity=0.7,
+                    marker={
+                        'size': 15,
+                        'line': {'width': 0.5, 'color': 'white'}
+                    },
+                    name=i
+                ) for i in df.continent.unique()
             ],
-            'layout': {
-                'title': 'Dash Data Visualization'
-            }
+            'layout': go.Layout(
+                xaxis={'title': 'GDP Per Capita'},
+                yaxis={'type': 'log', 'title': 'Population'},
+                margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
+                legend={'x': 0, 'y': 1},
+                hovermode='closest'
+            )
         }
     )
 ])
